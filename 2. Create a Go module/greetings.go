41cb79fd209ed288b5f20a65c8cc65794260353e
unknown <zulfikrimirza@yahoo.com>
2023-11-07 10:32:34 +0700

    learn testing

--- 2. Create a Go module/greetings.go
+++ 2. Create a Go module/greetings.go
@@ -11,10 +11,29 @@ func Hello(name string) (string, error) {
 		return "", errors.New("empty name")
 	}
 
-	message := fmt.Sprintf(randomFormat(), name)
+	// message := fmt.Sprintf(randomFormat(), name)
+	message := fmt.Sprintf(randomFomat())
 	return message, nil
 }
 
+//Greet many people at once
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+
+		messages[name] = message
+	}
+	return messages, nil
+}
+
+
+
+//Greet people with random format
 func randomFormat() string{
 	formats := []string{
 		"Hi, %v. Welcome!",
